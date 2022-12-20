1fc91664a4a43a0adb532e1cf49812f2f1c2b922
Johnson1_Chang <Johnson1_Chang@asus.com>
2022-12-20 23:27:42 +0800

    day12

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -1,7 +1,6 @@
 import java.awt.Point
 import java.io.File
 import java.lang.IllegalArgumentException
-import kotlin.math.abs
 
 enum class Dir(val dx: Int, val dy: Int) {
     UP(0, -1),
@@ -11,13 +10,17 @@ enum class Dir(val dx: Int, val dy: Int) {
 }
 
 fun main() {
-    class Int2dMap(val arr: MutableList<MutableList<Int>>) : List<List<Int>> by arr {
-        fun get(point: Point): Int? {
-            return try {
-                arr[point.y][point.x]
-            } catch (e: IndexOutOfBoundsException) {
+    class TwoDimArr(val arr: Array<Int?>, val stride: Int) {
+        operator fun get(p: Point): Int? {
+            return if (p.x < 0 || p.x >= stride || p.y < 0 || p.y >= arr.size / stride) {
                 null
+            } else {
+                arr[p.y * stride + p.x]
             }
+        }
+
+        operator fun set(p: Point, v: Int?) {
+            arr[p.y * stride + p.x] = v
         }
     }
 
@@ -27,38 +30,47 @@ fun main() {
 
     fun loadData() = File("src/main/resources/Day12.txt").readLines()
 
-    fun loadHeightMap(): Triple<Int2dMap, Point, Point> {
+    fun loadMap(): Triple<TwoDimArr, Point, Point> {
         var start: Point? = null
         var end: Point? = null
-        val arr = loadData().mapIndexed { row, line ->
+        val heightMapStr = loadData()
+        val heightMap: Array<Int?> = heightMapStr.flatMapIndexed { row, line ->
             line.mapIndexed { col, char ->
                 when {
                     char.isLowerCase() -> char.code - aCode
                     char.toString() == "S" -> {
-                        start = Point(row, col)
+                        start = Point(col, row)
                         "a".code() - aCode
                     }
 
                     char.toString() == "E" -> {
-                        end = Point(row, col)
+                        end = Point(col, row)
                         "z".code() - aCode
                     }
 
                     else -> throw IllegalArgumentException("WTF")
                 }
-            }.toMutableList()
-        }.toMutableList()
-        return Triple(Int2dMap(arr), start!!, end!!)
+            }
+        }.toTypedArray()
+        return Triple(TwoDimArr(heightMap, heightMapStr.first().length), start!!, end!!)
     }
 
-    fun part1(): Int {
-        val (heightMap, startPoint, endPoint) = loadHeightMap()
+    val (heightMap, startPoint, endPoint) = loadMap()
+
+    fun Point.flatten() = this.y * heightMap.stride + this.x
+
+    fun run(
+        startPoint: Point,
+        climbableCriteria: (currentH: Int, nextH: Int) -> Boolean,
+        finishCriteria: (pioneers: List<Point>) -> Boolean,
+        plotFigure: Boolean = false
+    ): Int {
 
         fun Point.move(dir: Dir): Boolean {
-            val height = heightMap.get(this)!!
+            val height = heightMap[this]!!
             this.translate(dir.dx, dir.dy)
-            val nextHeight = heightMap.get(this)
-            return if (nextHeight != null && abs(nextHeight - height) <= 1) {
+            val nextHeight = heightMap[this]
+            return if (nextHeight != null && climbableCriteria.invoke(height, nextHeight)) {
                 true
             } else {
                 this.translate(-dir.dx, -dir.dy)
@@ -66,21 +78,45 @@ fun main() {
             }
         }
 
-        val stepsCountMap = Int2dMap(MutableList(heightMap.size) { IntArray(heightMap[it].size) { Int.MAX_VALUE }.toMutableList() })
-        stepsCountMap.arr[startPoint.y][startPoint.x] = 0
+        val stepsCountMap = TwoDimArr(Array(heightMap.arr.size) { null }, heightMap.stride)
+        stepsCountMap[startPoint] = 0
 
         val pioneers = mutableListOf(Point(startPoint))
         val newPioneers = mutableSetOf<Point>()
 
-        repeat(100) {
-            println("Round#$it: $pioneers")
+        fun plotSearchingStatus() {
+            val pioneerLocations = pioneers.map { it.flatten() }
+            val plot = stepsCountMap.arr.toList()
+                .mapIndexed { i, step ->
+                    when {
+                        i == startPoint.flatten() -> "S"
+                        i == endPoint.flatten() -> "E"
+                        i in pioneerLocations -> "#"
+                        step != null -> "o"
+                        else -> "."
+                    }
+                }
+                .joinToString("")
+                .chunked(stepsCountMap.stride)
+                .joinToString(separator = "\n")
+
+            plot.print()
+        }
+
+        repeat(times = 1000) { round ->
+            println("Round #$round: ${pioneers.joinToString(separator = ", ") { "(${it.x}, ${it.y}, h=${heightMap[it]})" }}")
+            if (plotFigure) {
+                plotSearchingStatus()
+                println()
+            }
+
             pioneers.forEach { pioneer ->
-                val currentStepCount = stepsCountMap.get(pioneer)!!
+                val currentStepCount = stepsCountMap[pioneer]!!
                 Dir.values().forEach { dir ->
                     val tester = Point(pioneer)
-                    if (tester.move(dir) && stepsCountMap.get(tester)!! > currentStepCount) {
+                    if (tester.move(dir) && (stepsCountMap[tester] == null || stepsCountMap[tester]!! > currentStepCount)) {
                         newPioneers.add(tester)
-                        stepsCountMap.arr[tester.y][tester.x] = currentStepCount + 1
+                        stepsCountMap[tester] = currentStepCount + 1
                     }
                 }
             }
@@ -88,14 +124,28 @@ fun main() {
             pioneers.addAll(newPioneers)
             newPioneers.clear()
 
-            if (stepsCountMap.get(endPoint)!! < Int.MAX_VALUE) {
-                return@repeat
+            if (finishCriteria.invoke(pioneers)) {
+                return round + 1
             }
         }
 
-        return 0
+        return -1
     }
 
-    println("part1 = ${part1()}")
-//    println("part2 = ${part2()}")
+    val part1 = run(
+        startPoint = startPoint,
+        climbableCriteria = { currentH, nextH -> currentH + 1 >= nextH },
+        finishCriteria = {pioneers -> endPoint in pioneers },
+        plotFigure = false
+    )
+
+    val part2 = run(
+        startPoint = endPoint,
+        climbableCriteria = {currentH, nextH -> currentH - 1 <= nextH },
+        finishCriteria = {pioneers -> pioneers.any { pioneer -> heightMap[pioneer] == 0 } },
+        plotFigure = true
+    )
+
+    println("part1 = $part1")
+    println("part2 = $part2")
 }
