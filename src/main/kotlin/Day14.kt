5cedebed3cf715fc426c5158365f0f805940161a
Johnson1_Chang <Johnson1_Chang@asus.com>
2022-12-21 19:54:15 +0800

    day14_method1

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -7,96 +7,120 @@ fun main() {
 
     fun loadData() = File("src/main/resources/Day14.txt").readLines()
 
-    class Cave {
-        val rocks: Set<Point>
+    class Cave(val withBaseLine: Boolean = false) {
+        val rocks = mutableSetOf<Point>()
         val sands = mutableSetOf<Point>()
+        val baseLine: Int by lazy { rocks.maxOf { it.y } + 2 }
 
         init {
-            val rockMutableSet = mutableSetOf<Point>()
-            loadData().forEach { path ->
-                path.split(" -> ").map { it.split(",").map { n -> n.toInt() } }.windowed(size = 2) { (rockStartP, rockEndP) ->
-                    val rocksInPath = if (rockStartP[0] == rockEndP[0]) {
-                        val x = rockStartP[0]
-                        val from: Int = min(rockStartP[1], rockEndP[1])
-                        val to: Int = max(rockStartP[1], rockEndP[1])
-                        (from..to).map { y -> Point(x, y) }
-                    } else if (rockStartP[1] == rockEndP[1]) {
-                        val y = rockStartP[1]
-                        val from: Int = min(rockStartP[0], rockEndP[0])
-                        val to: Int = max(rockStartP[0], rockEndP[0])
-                        (from..to).map { x -> Point(x, y) }
-                    } else {
-                        throw IllegalArgumentException("WTF")
-                    }
-                    rockMutableSet.addAll(rocksInPath)
+            loadData().forEach { path -> addRockPath(path) }
+        }
+
+        fun addRockPath(path: String) {
+            path.split(" -> ").map { it.split(",").map { n -> n.toInt() } }.windowed(size = 2) { (rockStartP, rockEndP) ->
+                val rocksInPath = if (rockStartP[0] == rockEndP[0]) {
+                    val x = rockStartP[0]
+                    val from: Int = min(rockStartP[1], rockEndP[1])
+                    val to: Int = max(rockStartP[1], rockEndP[1])
+                    (from..to).map { y -> Point(x, y) }
+                } else if (rockStartP[1] == rockEndP[1]) {
+                    val y = rockStartP[1]
+                    val from: Int = min(rockStartP[0], rockEndP[0])
+                    val to: Int = max(rockStartP[0], rockEndP[0])
+                    (from..to).map { x -> Point(x, y) }
+                } else {
+                    throw IllegalArgumentException("WTF")
                 }
+                rocks.addAll(rocksInPath)
             }
-            rocks = rockMutableSet.toSet()
         }
 
         fun plot() {
-            val xStart = rocks.minOf { it.x }
-            val xEnd = rocks.maxOf { it.x }
-            val yStart = rocks.minOf { it.y }
-            val yEnd = rocks.maxOf { it.y }
+            val particles = rocks.union(sands)
+
+            val xStart = particles.minOf { it.x }
+            val xEnd = particles.maxOf { it.x }
+            val yStart = particles.minOf { it.y }
+            val yEnd = if (withBaseLine) baseLine else particles.maxOf { it.y }
             val figure = Array(yEnd - yStart + 1) { Array(xEnd - xStart + 1) { "." } }
 
             rocks.forEach { figure[it.y - yStart][it.x - xStart] = "#" }
             sands.forEach { figure[it.y - yStart][it.x - xStart] = "o" }
+            if (withBaseLine) {
+                (xStart..xEnd).forEach { x -> figure[baseLine - yStart][x - xStart] = "#" }
+            }
 
-            figure.joinToString(separator = "\n") { it.joinToString(separator = "") }.log()
+            figure.joinToString(separator = "\n", postfix = "\n") { it.joinToString(separator = "") }.log()
         }
 
-        fun dropSandFrom(sand: Point) {
+        fun dropSandFrom(sand: Point): Boolean {
+            val particles = rocks.union(sands)
+
             while (true) {
-                if (sand.apply { translate(0, 1) }.let { it !in rocks && it !in sands }) {
+                val targetHit = particles.filter { it.x == sand.x && it.y > sand.y }.minByOrNull { it.y } ?: if (withBaseLine) {
+                    sand.move(sand.x, baseLine - 1)
+                    sands.add(sand)
+                    return true
+                } else {
+                    return false
+                }
+
+                if (sand.y < targetHit.y - 1) {
+                    sand.move(targetHit.x, targetHit.y - 1)
                     continue
                 }
-                sand.translate(0, -1)
 
-                if (sand.apply { translate(-1, 1) }.let { it !in rocks && it !in sands }) {
+                if (sand.apply { translate(-1, 1) } !in particles) {
                     continue
                 }
                 sand.translate(1, -1)
 
-                if (sand.apply { translate(1, 1) }.let { it !in rocks && it !in sands }) {
+                if (sand.apply { translate(1, 1) } !in particles) {
                     continue
                 }
                 sand.translate(-1, -1)
 
                 sands.add(Point(sand))
-                break
+                return true
             }
         }
-    }
-
-    fun packedCalories(data: List<String>): MutableList<MutableList<Int>> {
-        val calories = mutableListOf<MutableList<Int>>()
-        data.forEach {
-            val cal = it.toIntOrNull()
-            if (cal != null) {
-                if (calories.isEmpty()) {
-                    calories.add(mutableListOf())
-                }
-                calories.last().add(cal)
-            } else {
-                calories.add(mutableListOf())
-            }
-        }
-        return calories
     }
 
     fun part1(): Int {
         val cave = Cave()
-        repeat(10) { round ->
-            "\nRound #$round".log()
-            cave.plot()
-            cave.dropSandFrom(Point(500, 0))
+
+        var round = 1
+        while (true) {
+//            "===== Round #$round =====".log()
+//            cave.plot()
+            if (!cave.dropSandFrom(Point(500, 0))) {
+                break
+            }
+            round++
+        }
+        cave.plot()
+
+        return cave.sands.size
+    }
+
+    fun part2(): Int {
+        val cave = Cave(withBaseLine = true)
+
+        val sandSource = Point(500, 0)
+        var round = 1
+        while (true) {
+            "===== Round #$round =====".log()
+//            cave.plot()
+            cave.dropSandFrom(Point(sandSource))
+
+            if (sandSource in cave.sands) {
+                break
+            }
+
+            round++
         }
 
-        "\nFinal".log()
-        cave.plot()
-        return 0
+        return cave.sands.size
     }
 
     println("part1 = ${part1()}")
