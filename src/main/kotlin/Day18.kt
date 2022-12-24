a7f37ba7783a6cd98984cb4e3d6375c441320eef
Johnson1_Chang <Johnson1_Chang@asus.com>
2022-12-25 01:01:06 +0800

    day18

--- src/main/kotlin/Day18.kt
+++ src/main/kotlin/Day18.kt
@@ -1,4 +1,6 @@
 import java.io.File
+import kotlin.math.max
+import kotlin.math.min
 
 fun main() {
 
@@ -6,10 +8,7 @@ fun main() {
 
     fun loadPts() = loadData().map { it.split(",").map { i -> i.toInt() } }
 
-    fun part1(): Int {
-
-        val pts = loadPts()
-
+    fun calcSurfaces(pts: List<List<Int>>): Int {
         fun calcConnectionAlongAxis(axis: Int): Int {
             val axisLeft = mutableListOf(0, 1, 2).apply { remove(axis) }
 
@@ -37,8 +36,84 @@ fun main() {
         return pts.size * 6 - 2 * (xConnection + yConnection + zConnection)
     }
 
+    fun fillAirPtsOutside(pts: List<List<Int>>): Pair<List<List<Int>>, List<Int>> {
+
+        val xMax = pts.maxOf { it[0] } + 1
+        val xMin = pts.minOf { it[0] } - 1
+        val yMax = pts.maxOf { it[1] } + 1
+        val yMin = pts.minOf { it[1] } - 1
+        val zMax = pts.maxOf { it[2] } + 1
+        val zMin = pts.minOf { it[2] } - 1
+
+        val lavaPtsMapAlongX = pts.groupBy { it[0] }
+
+        val airPtsMapAlongX = mutableMapOf<Int, MutableSet<List<Int>>>()
+
+        val nearbyOffsets = listOf(
+            listOf(-1, 0, 0),
+            listOf(1, 0, 0),
+            listOf(0, -1, 0),
+            listOf(0, 1, 0),
+            listOf(0, 0, -1),
+            listOf(0, 0, 1)
+        )
+
+        do {
+            val airPtsCount = airPtsMapAlongX.values.flatten().count()
+
+            "$airPtsCount air units were filled outside.".logi()
+
+            (xMin..xMax).forEach { x ->
+                airPtsMapAlongX.putIfAbsent(x, mutableSetOf())
+
+                if (x == xMin && airPtsMapAlongX[x]!!.isEmpty()) {
+                    (yMin..yMax).forEach { y ->
+                        (zMin..zMax).forEach { z ->
+                            airPtsMapAlongX[x]!!.add(listOf(x, y, z))
+                        }
+                    }
+                    return@forEach
+                }
+
+                val nearbyMinX = max(x - 1, airPtsMapAlongX.keys.min())
+                val nearbyMaxX = min(x + 1, airPtsMapAlongX.keys.max())
+
+                val airNearbyPts = (nearbyMinX..nearbyMaxX).map { airPtsMapAlongX[it]!! }.flatten()
+                val airPtsInX = airPtsMapAlongX[x]!!
+                val lavaPtsInX = lavaPtsMapAlongX[x]
+
+                (yMin..yMax).forEach { y ->
+                    (zMin..zMax).forEach { z ->
+                        val p = listOf(x, y, z)
+                        if (lavaPtsInX?.contains(p) != true
+                            && p !in airPtsInX
+                            && nearbyOffsets.map { p.zip(it) { a, offset -> a + offset } }.any { it in airNearbyPts }
+                        ) {
+                            airPtsInX.add(p)
+                        }
+                    }
+                }
+            }
+        } while (airPtsCount != airPtsMapAlongX.values.flatten().count())
+
+        return Pair(airPtsMapAlongX.values.flatten(), listOf(xMax - xMin + 1, yMax - yMin + 1, zMax - zMin + 1))
+    }
+
+    fun part1(): Int {
+        return calcSurfaces(loadPts())
+    }
+
+    fun part2(): Int {
+        val lavaPts = loadPts()
+        val (airPts, squareSize) = fillAirPtsOutside(lavaPts)
+        val airSurface = calcSurfaces(airPts)
+        val (xSize, ySize, zSize) = squareSize
+        "Air cube size = $squareSize".logi()
+        return airSurface - 2 * (xSize * ySize + ySize * zSize + zSize * xSize)
+    }
+
     val part1 = part1()
-//    val part2 = part2(logLevel = 2)
+    val part2 = part2()
     println("part1 = $part1")
-//    println("part2 = $part2")
+    println("part2 = $part2")
 }
