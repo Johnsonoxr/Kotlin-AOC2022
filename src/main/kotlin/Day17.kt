7bd8f6ed1ec3f7c2b2d12547208736fc802e3c66
Johnson1_Chang <Johnson1_Chang@asus.com>
2022-12-24 18:52:39 +0800

    day17

--- src/main/kotlin/Day17.kt
+++ src/main/kotlin/Day17.kt
@@ -1,21 +1,23 @@
 import java.io.File
+import kotlin.math.log
+import kotlin.math.pow
 
 fun main() {
 
     class Rock(vararg yxPairs: Int) {
-        val pts: Map<Int, Set<Int>>
+        val pts: Map<Int, Int>
         val width: Int
         val height: Int
 
         init {
-            val ml = mutableMapOf<Int, MutableSet<Int>>()
+            val ml = mutableMapOf<Int, Int>()
             yxPairs.toList().chunked(2).forEach { (y, x) ->
-                ml.putIfAbsent(y, mutableSetOf())
-                ml[y]!!.add(x)
+                ml.putIfAbsent(y, 0)
+                ml[y] = ml[y]!! + 2.toDouble().pow(x).toInt()
             }
             pts = ml.toMap()
-            width = ml.values.flatten().max() - ml.values.flatten().min() + 1
-            height = ml.keys.max() - ml.keys.min() + 1
+            width = log(ml.values.max().toDouble(), 2.0).toInt() + 1
+            height = ml.keys.max() + 1
         }
     }
 
@@ -29,7 +31,7 @@ fun main() {
 
     class Chamber {
         val width = 7
-        val rockMap = mutableMapOf<Long, MutableSet<Int>>()
+        val rockMap = mutableMapOf<Long, Int>()
 
         fun canFitInWith(rock: Rock, y: Long, x: Int): Boolean {
             if (x < 0 || (x + rock.width) > width || y < 0) {
@@ -39,23 +41,41 @@ fun main() {
                 return true
             }
 
-            return !rock.pts.any { (rockY, rockXs) -> rockMap[rockY + y]?.any { (it - x) in rockXs } == true }
+            return rock.pts.all { (rockY, rockX) -> (rockMap[rockY + y] ?: return@all true) and (rockX shl x) == 0 }
         }
 
         fun addRock(rock: Rock, y: Long, x: Int) {
             rock.pts.forEach { (rockY, rockXs) ->
                 val depth = y + rockY
-                rockMap.putIfAbsent(depth, mutableSetOf())
-                rockMap[depth]!!.addAll(rockXs.map { it + x })
+                rockMap.putIfAbsent(depth, 0)
+                rockMap[depth] = rockMap[depth]!! or (rockXs shl x)
+            }
+
+            if (rockMap.size > 100) {
+                clearRedundantLayers()
             }
         }
 
-        fun dropLayers(fromY: Long) {
-            rockMap.entries.removeIf { it.key < fromY }
+        fun clearRedundantLayers() {
+            val chamberTop = rockMap.keys.maxOrNull() ?: return
+
+            val layerTested = 3
+            var layerRemoved = false
+
+            (chamberTop - 10 until chamberTop).reversed().windowed(layerTested).forEach { layerIndices ->
+                if (layerRemoved) {
+                    return@forEach
+                }
+                if (layerIndices.map { rockMap[it]!! }.reduce { acc, i -> acc or i } == (2.0.pow(width).toInt()) - 1) {
+                    rockMap.entries.removeIf { it.key < layerIndices.last() - 1 }
+                    layerRemoved = true
+                    "Remove layer data below ${layerIndices.last() - 1}".logd()
+                }
+            }
         }
     }
 
-    fun loadData() = File("src/main/resources/Day17_test.txt").readLines()
+    fun loadData() = File("src/main/resources/Day17.txt").readLines()
 
     fun loadWindDx() = loadData()[0].map {
         when {
@@ -74,14 +94,15 @@ fun main() {
         var windIdx = 0
         var chamberTop = 0L
 
-        repeat(times = 2022) {rockIdx ->
+        repeat(times = 2022) { rockIdx ->
             val rock = rocks[rockIdx % rocks.size]
 
             var rockY = 3L + chamberTop
             var rockX = 2
 
             while (true) {
-                val windDx = winds[windIdx++ % winds.size]
+                val windDx = winds[windIdx]
+                windIdx = (windIdx + 1) % winds.size
 
                 if (chamber.canFitInWith(rock, rockY, rockX + windDx)) {
                     rockX += windDx
@@ -111,8 +132,96 @@ fun main() {
         return chamberTop
     }
 
+    fun part2(logLevel: Int = 2): Long {
+        myLogLevel = logLevel
+
+        val winds = loadWindDx()
+        val chamber = Chamber()
+
+        val totalRockCount = 1000000000000
+
+        var windIdx = 0
+        var chamberTop = 0L
+
+        var patternMap: MutableMap<Rock, MutableSet<Int>>? = null
+        var rockIdxInMonitor = 0L
+        var recordedChamberTop = 0L
+        var skipped = false
+
+        var rockIdx = 0L
+        while (rockIdx < totalRockCount) {
+            val rock = rocks[(rockIdx % rocks.size).toInt()]
+
+            if (rockIdx > 100000 && !skipped) {
+                if (patternMap == null) {
+                    "Start monitoring pattern at Rock #$rockIdx".logi()
+                    patternMap = mutableMapOf()
+                    rockIdxInMonitor = rockIdx
+                    recordedChamberTop = chamberTop
+                }
+
+                patternMap.putIfAbsent(rock, mutableSetOf())
+
+                val windIndicesOfRock = patternMap[rock]!!
+                if (windIdx in windIndicesOfRock) {
+                    val cycleOfRocks = rockIdx - rockIdxInMonitor
+                    val cycleOfYOffset = chamberTop - recordedChamberTop
+
+                    val skipCycle = (totalRockCount - rockIdx) / cycleOfRocks
+                    val skippedRocks = skipCycle * cycleOfRocks
+                    val skippedOffset = skipCycle * cycleOfYOffset
+                    skipped = true
+
+                    rockIdx += skippedRocks
+                    chamberTop += skippedOffset
+                    chamber.rockMap.putAll(chamber.rockMap.map { it.key + skippedOffset to it.value })
+
+                    ("Pattern found within cycle of $cycleOfRocks rocks along with y-offset $cycleOfYOffset," +
+                            " skip $skippedRocks rocks and $skippedOffset y-offset. ${totalRockCount - rockIdx} rocks left.").logi()
+                } else {
+                    windIndicesOfRock.add(windIdx)
+                }
+            }
+
+            var rockY = 3L + chamberTop
+            var rockX = 2
+
+            while (true) {
+                val windDx = winds[windIdx]
+                windIdx = (windIdx + 1) % winds.size
+
+                if (chamber.canFitInWith(rock, rockY, rockX + windDx)) {
+                    rockX += windDx
+                    "Wind blow [$windDx]".logv()
+                } else {
+                    "Wind blow [$windDx] in vain".logv()
+                }
+
+                val gravityDy = -1
+                if (chamber.canFitInWith(rock, rockY + gravityDy, rockX)) {
+                    rockY += gravityDy
+                    "Gravity pull [$gravityDy]".logv()
+                } else {
+                    chamber.addRock(rock, rockY, rockX)
+                    chamberTop = (chamber.rockMap.keys.maxOrNull() ?: -1L) + 1L
+                    "Rock #$rockIdx lands on ($rockX, $rockY), chamber's top is $chamberTop".logd()
+                    break
+                }
+
+                "Rock position = ($rockX, $rockY)".logv()
+            }
+
+            rockIdx++
+
+            "".logv()
+        }
+
+        "Done: top of chamber is $chamberTop".logi()
+        return chamberTop
+    }
+
     val part1 = part1(logLevel = 2)
-//    val part2 = tetris(1_000_000_000_000L, logLevel = 2)
+    val part2 = part2(logLevel = 2)
     println("part1 = $part1")
-//    println("part2 = $part2")
+    println("part2 = $part2")
 }
